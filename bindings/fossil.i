c3afadc3fc4fc31cccbbe9e1076d91b634692fc2
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2013-07-17 17:06:21 +0200

    yay, we have some access to historical_version_of_file

--- bindings/fossil.i
+++ bindings/fossil.i
@@ -1,13 +1,16 @@
 %module fossil
 
- // We wrap very little yet, just a fragment of Blobs.
+%include <argcargv.i>
+
+ // We wrap very little yet
 
 %{
 #ifdef NORETURN
 #undef NORETURN
 #endif
 #include "src/config.h"
-#include "blob.h"
+#include "src/th.h"
+#include "_accum_.h"
 %}
 
 %define NORETURN
@@ -15,8 +18,19 @@
 
 %ignore blob_vappendf;
 %ignore vxprintf;
+%ignore vmprintf;
+%ignore db_vprepare;
 
-%include <blob.h>
+// static
+%ignore db_sql_trace;
+%ignore file_is_selected;
+%ignore db_sql_cgi;
+%ignore db_sql_print;
+%ignore db_sql_user;
+%ignore db_open;
+%ignore cgi_vprintf;
+
+%include <_accum_.h>
 
  // Translation may have string leaks, not worried about that yet,
  // just need to add swig decorators to let it know what it should
@@ -44,5 +58,22 @@
 
   const char *__str__() {
     return blob_str($self);
+  }
+}
+
+%apply (int ARGC, char **ARGV) { (int argc, char **argv) } 
+%inline {
+  int main(int argc, char **argv) {
+    return fossil_main(argc,argv);;
+  }
+
+  int args(int argc, char **argv) {
+    memset(&g, 0, sizeof(g));
+    g.now = time(0);
+    // doesn't do utf8 stuff
+    g.argc = argc;
+    g.argv = argv;
+    sqlite3_initialize();
+    return 0;
   }
 }
--- bindings/test.rb
+++ bindings/test.rb
@@ -1,7 +1,8 @@
 #!/usr/bin/env ruby
 
 
-# remember to set RUBYLIB, e.g. RUBYLIB=$PWD ruby example.rb from build dir
+# remember to set RUBYLIB, e.g. from a "build" directory within source:
+#   RUBYLIB=$PWD/lib ruby ../bindings/test.rb
 
 require 'fossil'
 
@@ -10,3 +11,43 @@ puts "blob is [#{blob}]"
 blob.fromString("hello")
 puts "blob is [#{blob}]"
 # lots of other methods available as Fossil::blob_foo(blob,...)
+
+def fossil(args)
+  # Not dealing yet with fossil's internal state, so do "big"
+  # fossil calls in a separate process - not very useful I know
+  make_a_repo = fork do
+    Fossil::main(["fossil"].concat(args))
+  end
+  Process.waitpid(make_a_repo)
+end
+
+unless File.exist? "data.csv"
+  File.open("data.csv","w") do |file|
+    file.write("NAME,AGE\n")
+    file.write("Bob,99\n")
+    file.write("Sam,156\n")
+  end
+end
+
+unless File.exist? "test.fossil"
+  fossil(["new","test.fossil"])
+  fossil(["open","test.fossil"])
+  fossil(["add","data.csv"]);
+  fossil(["commit","-m","add some test data"]);
+  # fossil(["close"]) # not yet, unpatched fossil won't let us access
+  # files without a tree - fix is easy but not yet offered upstream
+end
+
+
+Fossil::args(["fossil","--repository","test.fossil"]) # not needed given fossil open
+Fossil::db_find_and_open_repository(0,0)
+puts "still here? yay! We have access to our repo."
+
+rc = Fossil::historical_version_of_file("tip","data.csv",blob,nil,nil,nil,0)
+puts "return code from historical_version_of_file is #{rc}"
+if rc==0
+  puts "failure, miserable failure"
+end
+puts blob
+
+# probably need to close some things down?
